46e68cb5c93fc0d1b6bb6644ebcf5fbbea18bce3
Srikanth Pragada <srikanthpragada@gmail.com>
2016-01-18 08:07:24 +0530

    Deletion of an element from Array

---
+++ array_delete.c
@@ -0,0 +1,33 @@
+// deletion of an element in an array
+main()
+{
+  int a[10];
+  int i,pos;
+
+  srand(time(0));
+  // fill it and display array
+  printf("\nBefore deletion\n");
+  for(i=0; i < 10; i++)
+  {
+      a[i] = rand()%100;
+      printf("%d\t", a[i]);
+  }
+  printf("\nEnter position :[0-9]");
+  scanf("%d",&pos);
+
+  // delete by pusing elements to right
+  for(i=pos; i <10;i++)
+  {
+      a[i] = a[i+1];
+  }
+
+  a[9]=0;
+
+  printf("\nAfter deletion\n");
+  for(i=0; i < 10; i++)
+  {
+
+      printf("%d\t", a[i]);
+  }
+
+}
