82537430105d4aca7a6b64d74a1fc186130a6288
Srikanth Pragada <srikanthpragada@gmail.com>
2018-05-23 16:09:09 +0530

    Concatenate names by separating them with comma(,)
    Take strings and concatenate them into a single string with , separating names

---
+++ concate_names.c
@@ -0,0 +1,25 @@
+// Program to concatenate names by separating them with , (comma)
+#include <stdio.h>
+
+main()
+{
+  char names[1000];
+  char name[50];
+
+    names[0] = '\0';  // start with empty string
+    while(1)
+    {
+      printf("Enter a name : ");
+      gets(name);
+
+      if (strcmp(name,"end") == 0)  // stop if name is "end"
+          break;
+
+      if (strlen(names) > 0)
+          strcat(names,",");   // add , only for second string onwards
+
+      strcat(names,name);  // add name at the end of names 
+    }
+
+    puts(names);
+}
