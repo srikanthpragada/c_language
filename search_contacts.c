245e717de6c9cfa19f92d018b41c5948c999e06c
Srikanth Pragada <srikanthpragada@gmail.com>
2018-10-27 19:45:00 +0530

    Search for contacts in contacts.dat
    Uses command line argument and searches binary file (contacts.dat).

---
+++ search_contacts.c
@@ -0,0 +1,37 @@
+// Search for contacts by take name on command line 
+// Uses file contacts.dat in current directory 
+
+# include <stdio.h>
+
+struct contact
+{
+   char name[30];
+   char email[50];
+   char mobile[15];
+};
+
+main(int argc, char * argv[])
+{
+   FILE * fp;
+   struct contact c;
+   int count;
+
+      if(argc < 2)
+      {
+          printf("Usage: search_contacts  <name>\n");
+          return;
+      }
+
+      fp = fopen("contacts.dat","rb");
+      while(1)
+      {
+        count = fread(&c,sizeof(c),1,fp);
+        if (count == 0)
+            break;
+
+        if (strstr(c.name, argv[1]) != NULL)
+           printf("%-20s %-30s  %s\n",c.name,c.email,c.mobile);
+      }
+
+      fclose(fp);
+}
